d807cd7c2b2636e6952c1fedd8cdb4c249e88547
Rami Elsayed <ramielsayed123@outlook.com>
2022-10-20 23:31:33 +0100

    printing instructions, empty board and board with play

--- tic-tac-toe/Program.cs
+++ tic-tac-toe/Program.cs
@@ -21,6 +21,53 @@
             }
             return choice;
         }
+        public static void PrintInstructions()
+        {
+            Console.WriteLine("       |      |       ");
+            Console.WriteLine("   x1  |  x2  |  x3   ");
+            Console.WriteLine(" ______|______|______ ");
+            Console.WriteLine("       |      |       ");
+            Console.WriteLine("   y1  |  y2  |  y3   ");
+            Console.WriteLine(" ______|______|______ ");
+            Console.WriteLine("       |      |       ");
+            Console.WriteLine("   z1  |  z2  |  z3   ");
+            Console.WriteLine("       |      |       ");
+            Console.WriteLine("Type one of the moves above");
+        }
+        public static void PrintEmptyBoard()
+        {
+            Console.WriteLine("      |     |      ");
+            Console.WriteLine("      |     |      ");
+            Console.WriteLine(" _____|_____|_____ ");
+            Console.WriteLine("      |     |      ");
+            Console.WriteLine("      |     |      ");
+            Console.WriteLine(" _____|_____|_____ ");
+            Console.WriteLine("      |     |      ");
+            Console.WriteLine("      |     |      ");
+            Console.WriteLine("      |     |      ");
+        }
+        public static void PrintBoard(Board board)
+        {
+            Console.WriteLine("      |     |      ");
+            PrintValueLine(board, "x", 2);
+            Console.WriteLine(" _____|_____|_____ ");
+            Console.WriteLine("      |     |      ");
+            PrintValueLine(board, "y", 2);
+            Console.WriteLine(" _____|_____|_____ ");
+            Console.WriteLine("      |     |      ");
+            PrintValueLine(board, "z", 2);
+            Console.WriteLine("      |     |      ");
+        }
+        public static void PrintValueLine(Board board, string s, int spacing)
+        {
+            var one = board.GetValue($"{s}1");
+            var two = board.GetValue($"{s}2");
+            var three = board.GetValue($"{s}3");
+
+            var spaces = string.Join("", Enumerable.Repeat(" ", spacing));
+            var line = $"{spaces}{spaces}{one}{spaces}|{spaces}{two}{spaces}|{spaces}{three}{spaces}";
+            Console.WriteLine(line);
+        }
     }
     
 }
